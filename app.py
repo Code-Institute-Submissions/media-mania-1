f6e2cb0668fe214f8fbbea7284f92a0a3aa9d9d8
adelliott92 <andrewdavidelliott@outlook.com>
2021-11-11 00:39:10 +0000

    :sparkles: feat: add navigation bar to base.html
    - Create navigation bar for application
    - Configure app routes in app.py for navigation bar links

--- app.py
+++ app.py
@@ -2,9 +2,20 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+# Homepage
 @app.route('/')
 def home():
     return render_template('index.html')
+
+# About us page
+@app.route('/about-us')
+def about():
+    return render_template('about.html')
+
+# Sign up / Login page
+@app.route('/sign-in')
+def signin():
+    return render_template('signin.html')
 
 if __name__ == "__main__":
     app.run()
